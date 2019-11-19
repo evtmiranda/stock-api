482a672775f57c9864c8fc543c17d56407c6321b
Everton Oliveira <notreve.miranda@gmail.com>
2019-11-19 16:59:07 -0300

    [FEAT] Add report methods

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -64,7 +64,7 @@ const getByFilters = async (filters) => {
 
 const getQuantityByStatus = async (filters) => {
     const stockStatuses = sequelize.query(
-    `SELECT status.description,
+        `SELECT status.description,
      COUNT(*) AS quantity 
      FROM stocks AS s 
      INNER JOIN stock_status AS ss ON s.id = ss.stock_id 
@@ -72,10 +72,59 @@ const getQuantityByStatus = async (filters) => {
      GROUP BY status.description`
     )
 
-   return stockStatuses
+    return stockStatuses
 }
 
-const remove =  async (id) => {
+const getQuantityByClient = async () => {
+    const query = `SELECT
+                        store,
+                        COUNT(*) AS quantity 
+                    FROM stocks
+                    WHERE deleted_at IS NULL
+                    AND TO_CHAR(created_at, 'yyyy-mm') = TO_CHAR(current_date, 'yyyy-mm')
+                    GROUP BY
+                        store`
+
+    const stocks = sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+
+    return stocks
+}
+
+const getEntryAndOutQuantityByDay = async () => {
+    const query = `WITH entry AS(
+                        SELECT
+                            TO_CHAR(entry_date, 'yyyy-mm-dd') AS entry_date,
+                            SUM(quantity) AS entry_quantity
+                        FROM stocks
+                        WHERE deleted_at IS NULL
+                        GROUP BY
+                            1
+                    ), output AS(
+                        SELECT
+                            TO_CHAR(output_date, 'yyyy-mm-dd') AS output_date,
+                            SUM(output_quantity) AS output_quantity
+                        FROM stocks
+                        WHERE deleted_at IS NULL
+                        GROUP BY
+                            1
+                    )
+                    SELECT
+                        TO_CHAR(DATE(e.entry_date), 'dd') AS day,
+                        e.entry_quantity AS "entryQuantity",
+                        COALESCE(o.output_quantity, 0) AS "outputQuantity"
+                    FROM entry AS e
+                    LEFT JOIN output AS o
+                    ON e.entry_date = o.output_date
+                    WHERE TO_CHAR(DATE(e.entry_date), 'yyyy-mm') = TO_CHAR(current_date, 'yyyy-mm')
+                    ORDER BY 
+                        1`
+
+    const stocks = sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+
+    return stocks
+}
+
+const remove = async (id) => {
     const result = await Stock.update({
         deletedAt: new Date()
     },
@@ -125,6 +174,8 @@ module.exports = {
     findAndFilter,
     getByFilters,
     getQuantityByStatus,
+    getQuantityByClient,
+    getEntryAndOutQuantityByDay,
     remove,
     update,
     create,
